1c53f98cd6d471a58bfef65c6cf15ea2f0e41414
supriya1912 <supriya191219@gmail.com>
2019-02-17 00:03:47 +0530

    add commit

---
+++ first.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    string s;
+    i = 9;
+    s = "supriya" ;
+    cout << i << " , " << s << endl;
+    return 0;
+
+}
